b7aed5ac5584c4a877e4b2d87bca7923c36d038c
tharun612 <123954376+tharun612@users.noreply.github.com>
2023-11-24 11:14:58 +0530

    Update Tharun.tf

--- Tharun.tf
+++ Tharun.tf
@@ -1,24 +1,23 @@
-
 provider "aws" { 
-  access_key = "AKIAVN2D6J7GC4S6KV7Z"
-  secret_key = "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"
-  region = "ap-south-1"
+  access_key = "AKIAW6KT6TBG5LIPYIP2"
+  secret_key = "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" 
+  region = "us-east-1" 
 } 
 resource "aws_instance" "myinstance" { 
-  ami = "ami-0287a05f0ef0e9d9a" 
+  ami = "ami-0fc5d935ebf8bc3bc" 
   instance_type = "t2.micro" 
-  availability_zone = "ap-south-1a" 
-  subnet_id = "subnet-0a6fdb0c9acd901f7" 
-  vpc_security_group_ids = ["sg-065da31091aed4761"] 
-  key_name = "kohli" 
-  associate_public_ip_address = true 
-  count = 1 
+  availability_zone = "us-east-1a"
+  subnet_id = "subnet-09022726d605f2f37" 
+  vpc_security_group_ids = ["sg-070848893435179da"]
+  key_name = "bhanu" 
+  associate_public_ip_address = true
+  count = 1  
   tags = { 
-  Name="kohli" 
+    Name="tharun"
   } 
   ebs_block_device { 
-   device_name = "/dev/sdb" 
-   volume_type = "gp2" 
-   volume_size = 10
-  } 
+  device_name = "/dev/sdb" 
+  volume_type = "gp2" 
+  volume_size = 10
+  }
 }

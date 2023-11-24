01f9cf520edb8ef5dbec7856ac6be248e6f82720
tharun612 <123954376+tharun612@users.noreply.github.com>
2023-11-24 09:26:30 +0530

    Update Tharun.tf

--- Tharun.tf
+++ Tharun.tf
@@ -2,12 +2,12 @@
 provider "aws" { 
   access_key = "AKIAVN2D6J7GC4S6KV7Z"
   secret_key = "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"
-  region = "us-east-1"
+  region = "ap-south-1"
 } 
 resource "aws_instance" "myinstance" { 
   ami = "ami-0fc5d935ebf8bc3bc" 
   instance_type = "t2.micro" 
-  availability_zone = "us-east-1a" 
+  availability_zone = "ap-south-1a" 
   subnet_id = "subnet-01d6f82fe66937006" 
   vpc_security_group_ids = ["sg-003933068c0bd85eb"] 
   key_name = "tharun" 
